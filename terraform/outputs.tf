cfbd9e33615e99582ef773ea7a2e73ffbae5468c
Imran-if <imran6622.if@gmail.com>
2026-03-28 21:04:30 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,6 @@ output "artifact_bucket_name" {
   description = "Name of the S3 artifact bucket"
   value       = aws_s3_bucket.artifact_store.bucket
 }
+output "jenkins_url" {
+  value = "http://${aws_instance.jenkins_server.public_ip}:8080"
+}
